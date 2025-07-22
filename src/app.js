df6758940ae98a79a93d9bbb6f4ea8b0b0c62d3e
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-07-22 12:04:51 +0530

    wroking on routes and route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req,res)=>{
-    res.send("hello test")
-})
 
 app.use("/hello", (req,res)=>{
-    res.send("hello hello")
+    res.send("hello hello");
 })
+
+app.use("/test", (req,res)=>{
+    res.send("hello test");
+})
+
+app.use("/", (req,res)=>{
+    res.send("namaste siva");
+})
+
+
+
 
 app.listen(3000, ()=>{
     console.group("server is running")
