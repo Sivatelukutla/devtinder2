a0c820ca014b9446abf63c754b92f8cefc959992
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-07-23 17:56:19 +0530

    create schema and model

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const connectDB = async()=>{
     await mongoose.connect("mongodb+srv://sivaprasadtelukutla:siva1358@namastenode4.wh0gzdo.mongodb.net/devTinder")
+
+
 }
 
 module.exports = connectDB
