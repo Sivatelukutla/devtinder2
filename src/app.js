4cf67463b10e276915669ae628c3c701b7fa39cc
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-07-29 11:28:54 +0530

    wroking on APIS

--- src/app.js
+++ src/app.js
@@ -3,28 +3,76 @@ const connectDB = require("./config/database");
 const user = require("./models/user.js")
 const app = express();
 
+app.use(express.json())
 
-
-app.post("/user", async(req,res)=>{
-  let userObj = {
-    firstName : "siva",
-    lastName : "telukutla",
-    email : "sivaprasadtelukutla@gmail.com",
-    password : "1358",
-    age : 1358,
-    gender : "male"
-  }
+app.get("/get", async(req,res)=>{
+  let data = req.body.email
   try{
-    let result = new user(userObj)
-    await result.save()
-    res.send("user is successfully")
+    let result = await user.find({
+    email : data
+  })
+  if(!data){
+    res.status(400).send("user is not found")
+  }
+  else{
+    res.send(result)
+  }
   }
   catch(err){
-    res.send(err)
+    res.status(400).send("user is not their")
   }
-
-
 })
+
+app.post("/user", async (req, res) => {
+  try {
+    let result = new user(req.body)
+    await result.save()
+    res.send(result);
+
+  }
+  catch (err) {
+    res.status(400).send("error in this code")
+  }
+})
+
+app.delete("/delete", async (req, res) => {
+  try {
+    let email = req.body.email
+    await user.findOneAndDelete({
+      email: email
+    })
+    res.send("user is deleted")
+  }
+  catch(err){
+    res.status(401).send(err)
+  }
+})
+
+// app.patch("/update", async (req, res) => {
+//   const { id, email } = req.body; // Get both `id` and `email` from the request body
+
+//   try {
+//     if (!id || !email) {
+//       return res.status(400).send("ID and email are required");
+//     }
+
+//     const result = await user.findByIdAndUpdate(
+//       id,
+//       { $set: { email } },
+//       { new: true }
+//     );
+
+//     if (!result) {
+//       return res.status(404).send("User not found");
+//     }
+
+//     res.send(result);
+//   } catch (err) {
+//     res.status(500).send("An error occurred while updating the user");
+//   }
+// });
+
+
 
 
 connectDB()
